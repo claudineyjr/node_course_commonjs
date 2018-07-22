1c58f09a634668ed6a44af7d6c2fc72a4fa9908d
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-22 15:46:53 -0300

    Exportando e importando módulos com CommonJs

---
+++ main.js
@@ -0,0 +1,5 @@
+const { add, multiplicate, personalInfo } = require('./resources-to-export'); // destructuring assignment
+
+console.log(add(1, 2));
+console.log(multiplicate(2, 3));
+console.log(personalInfo);
---
+++ resources-to-export.js
@@ -0,0 +1,14 @@
+const add = (x, y) => x + y;
+const multiplicate = (x, y) => x * y;
+
+const personalInfo = {
+  name: 'Claudiney Junior',
+  github: 'claudineyjr',
+  birthday: '1988-10-24'
+};
+
+module.exports = {
+  add,
+  multiplicate,
+  personalInfo,
+}
