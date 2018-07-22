282cacab1dcdc3220bd093096bc6cb8042aa26dd
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-22 15:52:45 -0300

    Exportando e importando módulos com CommonJs

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+const fullModule = require('./resources-to-export');
 const { add, multiplicate, personalInfo } = require('./resources-to-export'); // destructuring assignment
+
+console.log(fullModule.add(2, 5));
+console.log(fullModule.personalInfo);
 
 console.log(add(1, 2));
 console.log(multiplicate(2, 3));
